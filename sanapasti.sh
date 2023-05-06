01a6868ec34425598498e3a89798910dbd5bb03c
VinzelOps <88572751+VinzelOps@users.noreply.github.com>
2023-05-07 02:51:02 +0700

    Update sanapasti.sh

--- sanapasti.sh
+++ sanapasti.sh
@@ -24,6 +24,9 @@ feroxbuster=~/tools/feroxbuster
 #Fungsi penggunaan paramspider
 paramspider=~/tools/ParamSpider/paramspider.py
 
+#Fungsi penggunaan Google Dorks
+dorks_hunter=~/tools/dorks_hunter/dorks_hunter.py
+
 #Fungsi melakukan Httprobing
 HTTPXCALL="httpx -silent -no-color -random-agent -ports 80,81,300,443,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4443,4444,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8444,8500,8800,8834,8880,8881,8888,8983,9000,9001,9043,9060,9080,9090,9091,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,27201,32000,55440,55672"
 
@@ -186,7 +189,13 @@ mengintai(){
   assetfinder -subs-only $domain >> ./$domain/$foldername/$domain.txt
   duration=$SECONDS
   echo "Pengintaian Subdomain dengan Assetfinder Selesai dalam : $(($duration / 60)) menit dan $(($duration % 60)) detik." | notify -silent
+  
+  echo -e "${green}4.Mengintai dengan Dorks Hunter...${reset}"
+  python3 $tools/dorks_hunter/dorks_hunter.py -d $domain -o ./$domain/$foldername/dorks.txt
+  duration=$SECONDS
+  echo "Pengintaian dengan Dorks Hunter Selesai dalam : $(($duration / 60)) menit dan $(($duration % 60)) detik." | notify -silent
 }
+
 
 #Fungsi pengecekan sertifikat SSL
 searchcrtsh(){
@@ -599,6 +608,7 @@ fi
   mkdir ./$domain/$foldername
   mkdir ./$domain/$foldername/reports/
   mkdir ./$domain/$foldername/screenshots/
+  touch ./$domain/$foldername/dorks.txt
   touch ./$domain/$foldername/crtsh.txt
   touch ./$domain/$foldername/shuffledns.txt
   touch ./$domain/$foldername/cnames.txt
